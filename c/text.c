4d8875b7c7d076875c9604c46f6ec6f5d8eb5d16
shraddha-bhardwaj <shimar1bhardwaj0@gmail.com>
2020-10-17 12:35:52 +0530

    added a text.c program for finding the smallest no.

---
+++ c/text.c
@@ -0,0 +1,23 @@
+// a program for finding the smallest number amoung the four numbers input by the user
+
+#include <stdio.h>
+int main()
+{
+
+    int a, b, c, d, x = 0;
+    printf("enter no. 1  \n:  ");
+    scanf("%d", &a);
+    printf("enter no. 2 \n:  ");
+    scanf("%d", &b);
+    printf("enter no. 3 \n:  ");
+    scanf("%d", &c);
+    printf("enter no. 4  \n:  ");
+    scanf("%d", &d);
+
+    x = a < b ? a : b;
+    x = x < c ? x : c;
+    x = x < d ? x : d;
+    printf("the smallest no. is %d \n", x);
+
+    return 0;
+}
